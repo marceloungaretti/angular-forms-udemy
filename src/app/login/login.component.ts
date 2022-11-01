6871308d813441692a6ab74ff22848a5c73f6083
Marcelo Ungaretti <marcelo.ungaretti@gmail.com>
2022-11-01 20:37:29 -0300

    fim da aula 12

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,7 +13,7 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
 
   }
-  login(loginForm: NgForm) {
-    console.log(loginForm.value, loginForm.valid)
+  login(loginForm: NgForm, submit) {
+    console.log(loginForm.value, loginForm.valid, submit);
   }
 }
