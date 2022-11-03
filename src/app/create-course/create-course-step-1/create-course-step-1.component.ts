c4fd90870150b9f9d4f4859e02de1e0139f2fa55
Marcelo Ungaretti <marcelo.ungaretti@gmail.com>
2022-11-03 14:23:40 -0300

    fim da aula 30

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {CoursesService} from '../../services/courses.service';
-import {Observable} from 'rxjs';
-import {filter} from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CoursesService } from '../../services/courses.service';
+import { Observable } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'create-course-step-1',
@@ -10,6 +10,16 @@ import {filter} from 'rxjs/operators';
   styleUrls: ['./create-course-step-1.component.scss']
 })
 export class CreateCourseStep1Component implements OnInit {
+
+  form = this.fb.group({
+    title: ['', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(60)
+    ]]
+  });
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
 
