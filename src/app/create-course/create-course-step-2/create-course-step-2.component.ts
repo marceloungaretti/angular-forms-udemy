5959e69d4cfd38ea5aaeb308917dfd214772776b
Marcelo Ungaretti <marcelo.ungaretti@gmail.com>
2022-11-05 01:33:26 -0300

    fim da aula 36

--- src/app/create-course/create-course-step-2/create-course-step-2.component.ts
+++ src/app/create-course/create-course-step-2/create-course-step-2.component.ts
@@ -1,6 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'create-course-step-2',
@@ -10,16 +9,26 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 export class CreateCourseStep2Component implements OnInit {
 
   form = this.fb.group({
-    courseType: ['premium', Validators.required]
+    courseType: ['premium', Validators.required],
+    price: [null, [
+      Validators.required,
+      Validators.min(1),
+      Validators.max(9999),
+      Validators.pattern("[0-9]+")
+    ]]
   });
 
-  constructor(private fb: FormBuilder){}
-
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.form.valueChanges.subscribe(val => {
+      const priceControl = this.form.controls["price"];
 
-
-
+      if (val.courseType == 'free' && priceControl.enabled) {
+        priceControl.disable({ emitEvent: false });
+      } else if (val.courseType == 'premium' && priceControl.disabled) {
+        priceControl.enable({ emitEvent: false });
+      }
+    });
   }
-
 }
